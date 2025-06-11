9a7543164367e60068e423b20b464d83feec62bb
Jonathas-da-Silva-Ferreira <105074747+Jonathas-da-Silva-Ferreira@users.noreply.github.com>
2025-06-11 15:08:20 -0300

    new update

--- main.py
+++ main.py
@@ -16,4 +16,15 @@ def listar_produtos():
     conn = conectar()
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM estoque")
-    
+    produtos = cursor.fetchall()
+    conn.close()
+
+    print("\n Produtos em estoque:")
+    for p in produtos:
+        print(f"ID: {p[0]}, Nome: {p[1]}, Quantidade: {p[2]} | Preco: R$ {p[3]:.2f}")
+    print()
+
+def buscar_produto():
+    nome = input("Digite o nome do produto a buscar: ").strip()
+    conn = conectar()
+    cursor = conn.cursor()
